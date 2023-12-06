eb6a358b97ec7dc82896f819bab55cd0fa929296
Marcos <mvtgomes.ege@uesc.br>
2023-12-06 18:22:28 -0300

    implementado classe leitura

---
+++ COELHOltda/src/Faturas/Leitura.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+
+class Leitura {
+    int matricula;
+    double ultimaLeitura;
+    double penultimaLeitura;
+}
