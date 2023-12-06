d72c6472416fa0bf4aa4a70a2187a65b900b703f
Marcos <mvtgomes.ege@uesc.br>
2023-12-06 18:59:58 -0300

    leitura corrijido

--- COELHOltda/src/Leitura/Leituras.java
+++ COELHOltda/src/Leitura/Leituras.java
@@ -1,12 +1,12 @@
-
-
+package Leitura;
 import java.util.Date;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.ArrayList;
 
-class Leitura {
+public class Leituras {
     private int matricula;
     private double ultimaLeitura;
     private double penultimaLeitura;
 }
+
