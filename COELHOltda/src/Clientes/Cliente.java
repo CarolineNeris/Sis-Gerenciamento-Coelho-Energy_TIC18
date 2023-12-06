8700754e583ac076c7dbcda150386c73a51c888c
Carol <carolnerisfonseca@gmail.com>
2023-12-06 11:41:12 -0300

    Incluir cliente implementado com sucesso

--- COELHOltda/src/Clientes/Cliente.java
+++ COELHOltda/src/Clientes/Cliente.java
@@ -1,8 +1,15 @@
 package Clientes;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cliente {
     private String nome;
     private String cpf;
+    private static List<Cliente> clientes = new ArrayList<>(); // Declaração da lista de clientes como estática
+
+    public Cliente() { // Adição do construtor vazio
+    }
 
     public Cliente(String nome, String cpf) {
         this.nome = nome;
@@ -21,4 +28,10 @@ public class Cliente {
     public String getCpf() {
         return cpf;
     }
+
+    // Incluir cliente
+    public static void incluirCliente(Cliente cliente) {
+        clientes.add(cliente);
+    }
+
 }
